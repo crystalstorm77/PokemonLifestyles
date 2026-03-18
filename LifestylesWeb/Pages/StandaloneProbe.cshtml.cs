7564246b431380cd71dd7605d1e06b3ae62fa528
crystalstorm77 <crystalstorm77@gmail.com>
2026-03-18 12:28:14 +1100

    Shell 1

---
+++ LifestylesWeb/Pages/StandaloneProbe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LifestylesWeb.Pages
+{
+    public class StandaloneProbeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
